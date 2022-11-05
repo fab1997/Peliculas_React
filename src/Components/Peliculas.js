18d05993d420137bf387255806cfc5b09f648ebd
fab1997 <93455997+fab1997@users.noreply.github.com>
2022-11-04 22:12:10 -0300

    cambio de las clases

--- src/Components/Peliculas.js
+++ src/Components/Peliculas.js
@@ -8,7 +8,7 @@ function Peliculas() {
                     <h5 className="Jocker">Jocker</h5>
                     <p className="card-text">Arthur Fleck es un hombre ignorado por la sociedad, cuya motivación en la vida es hacer reír.
             Pero una serie de trágicos acontecimientos le llevarán a ver el mundo de otra forma...</p>
-                    <a href="./Peliculas/pelicua1.html" class="btn btn-primary">Ver Más</a>
+                    <a href="./Peliculas/pelicua1.html" className="btn btn-primary">Ver Más</a>
                 </div>
             </div>
 
@@ -18,7 +18,7 @@ function Peliculas() {
                     <h5 className="Vengadores: End Game">Vengadores: End Game</h5>
                     <p className="card-text">Después de los eventos devastadores de 'Vengadores: Infinity War', el universo está en ruinas debido a las
       acciones de Thanos...</p>
-                    <a href="./Peliculas/pelicula2.html" class="btn btn-primary">Ver Más</a>
+                    <a href="./Peliculas/pelicula2.html" className="btn btn-primary">Ver Más</a>
                 </div>
             </div>
 
@@ -29,7 +29,7 @@ function Peliculas() {
                     <p className="card-text">John Wick (Keanu Reeves) regresa a la acción, solo que esta vez con una recompensa de 14 millones de
             dólares
             sobre su cabeza y con un ejército de mercenarios intentando darle caza...</p>
-                    <a href="./Peliculas/pelicula3.html" class="btn btn-primary">Go somewhere</a>
+                    <a href="./Peliculas/pelicula3.html" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
 
@@ -40,7 +40,7 @@ function Peliculas() {
                     <p className="card-text">El capitán Jack Sparrow se enfrentará a un grupo de piratas-fantasma comandados por una de sus viejas
             némesis,
             el terrorífico capitán Salazar, recién escapado del Triángulo de las Bermudas...</p>
-                    <a href="./Peliculas/pelicula4.html" class="btn btn-primary">Ver Más</a>
+                    <a href="./Peliculas/pelicula4.html" className="btn btn-primary">Ver Más</a>
                 </div>
             </div>
 
@@ -50,7 +50,7 @@ function Peliculas() {
                     <h5 className="card-title">Rapidos y Furiosos</h5>
                     <p className="card-text">Una misteriosa banda de delincuentes se dedica a robar camiones en plena marcha desde vehículos
             deportivos...</p>
-                    <a href="./Peliculas/pelicula5.html" class="btn btn-primary">Ver Más</a>
+                    <a href="./Peliculas/pelicula5.html" className="btn btn-primary">Ver Más</a>
                 </div>
             </div>
         </div>
